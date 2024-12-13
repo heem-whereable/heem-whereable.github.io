efdd480893a52a75810a05ca7e2e46ebd456f1be
heem-whereable <heem-whereable@users.noreply.github.com>
2024-12-13 02:19:50 +0000

    deploy: whereable-ai/rplidar-diagnostic-tool@96bc3922a4bb6446ae20b38bb90c5af7559fa58f

--- rplidar_s_series_diagnostic_tool/html/search/functions_2.js
+++ rplidar_s_series_diagnostic_tool/html/search/functions_2.js
@@ -2,9 +2,9 @@ var searchData=
 [
   ['calculate_5fchecksum_329',['calculate_checksum',['../classlidar__controller_1_1LidarController.html#a6bc548fcca131e7ece3a5ecbdf83fab2',1,'lidar_controller::LidarController']]],
   ['calculate_5frpm_330',['calculate_rpm',['../classlidar__controller_1_1LidarController.html#a65a1f2da3569a04f097f78dcf84063a0',1,'lidar_controller::LidarController']]],
-  ['clear_5flidar_5flist_331',['clear_lidar_list',['../classcontrol__panel_1_1LidarControlPanel.html#a55ae7d67404ceda1a7b93388d38fa68a',1,'control_panel::LidarControlPanel']]],
-  ['clear_5flogs_332',['clear_logs',['../classstatus__monitor_1_1StatusMonitor.html#a50e3fa0b24f7ff82486a4f8a22d0042e',1,'status_monitor::StatusMonitor']]],
-  ['clear_5fpoints_333',['clear_points',['../classpoint__cloud__monitor_1_1PointCloudMonitor.html#a464d693a47f9ff663228846833403f0d',1,'point_cloud_monitor::PointCloudMonitor']]],
+  ['clear_331',['clear',['../classpoint__cloud__monitor_1_1PointCloudMonitor.html#abd51c1a72be9fdccf7b667664a85b20e',1,'point_cloud_monitor::PointCloudMonitor']]],
+  ['clear_5flidar_5flist_332',['clear_lidar_list',['../classcontrol__panel_1_1LidarControlPanel.html#a55ae7d67404ceda1a7b93388d38fa68a',1,'control_panel::LidarControlPanel']]],
+  ['clear_5flogs_333',['clear_logs',['../classstatus__monitor_1_1StatusMonitor.html#a50e3fa0b24f7ff82486a4f8a22d0042e',1,'status_monitor::StatusMonitor']]],
   ['clear_5ftmp_5fvalues_334',['clear_tmp_values',['../classlidar_1_1Lidar.html#a7bd08f18d97688f74d1f31e4342af1bf',1,'lidar::Lidar']]],
   ['close_335',['close',['../classlidar__controller_1_1LidarController.html#ad8d7171fd1daad016df5921dabc584fc',1,'lidar_controller::LidarController']]],
   ['close_5fapplication_336',['close_application',['../classmenu__bar_1_1MenuBarManager.html#a398abc212eb6946f9678ae8aad5fcdde',1,'menu_bar::MenuBarManager']]],
