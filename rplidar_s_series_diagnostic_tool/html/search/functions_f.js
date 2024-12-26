fd78de2955a74375b78c28a91c935124ca4c206c
heem-whereable <heem-whereable@users.noreply.github.com>
2024-12-26 01:40:06 +0000

    deploy: whereable-ai/rplidar-diagnosis-tool@f5e30759c2302a59b66a4eef96b5cf9fb349ef02

--- rplidar_s_series_diagnostic_tool/html/search/functions_f.js
+++ rplidar_s_series_diagnostic_tool/html/search/functions_f.js
@@ -1,5 +1,6 @@
 var searchData=
 [
   ['toggle_5fconnection_517',['toggle_connection',['../classcontrol__panel_1_1LidarControlPanel.html#aaee9fe653d873a7b0972e124132f1674',1,'control_panel::LidarControlPanel']]],
-  ['toggle_5ffull_5fscreen_518',['toggle_full_screen',['../classmenu__bar_1_1MenuBarManager.html#a2f9ce4cf9482a290abacbcb1850860bc',1,'menu_bar::MenuBarManager']]]
+  ['toggle_5ffull_5fscreen_518',['toggle_full_screen',['../classmenu__bar_1_1MenuBarManager.html#a2f9ce4cf9482a290abacbcb1850860bc',1,'menu_bar::MenuBarManager']]],
+  ['toggle_5fselected_5flidar_5fconnection_519',['toggle_selected_lidar_connection',['../classcontrol__panel_1_1LidarControlPanel.html#ada452dc0454281ed109ddf020dd3ae33',1,'control_panel::LidarControlPanel']]]
 ];
